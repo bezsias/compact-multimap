e7780702cb9dea7b78d4003543192de9493e9da9
Bela Ezsias <bezsias@hiya.com>
2016-11-02 11:13:20 +0100

    package visibility for Util

--- src/main/java/jmultimap/Util.java
+++ src/main/java/jmultimap/Util.java
@@ -2,11 +2,11 @@ package jmultimap;
 
 import java.io.*;
 
-public class Util {
-    private static int BUFFER_SIZE = 8192;
-    public static byte ZERO_BYTE   = (byte) 0;
+class Util {
+    static int BUFFER_SIZE = 8192;
+    static byte ZERO_BYTE   = (byte) 0;
 
-    public static void copy(InputStream from, OutputStream to)
+    static void copy(InputStream from, OutputStream to)
             throws IOException {
         byte[] buf = new byte[BUFFER_SIZE];
         int len;
@@ -14,12 +14,12 @@ public class Util {
             to.write(buf, 0, len);
     }
 
-    public static void writeShort(short value, byte[] array, int pos) {
+    static void writeShort(short value, byte[] array, int pos) {
         array[pos] = (byte)((value >> 8) & 0xFF);
         array[pos+1] = (byte)(value & 0xFF);
     }
 
-    public static short readShort(byte[] array, int pos) {
+    static short readShort(byte[] array, int pos) {
         return (short)((array[pos] & 0xFF) << 8 | (array[pos+1] & 0xFF));
     }
 }
